24ea175fee97399cb68218863f7a519f931384c2
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-08-23 17:06:38 +0200

    ministd.math : inRange

--- source/ministd/math.d
+++ source/ministd/math.d
@@ -2,6 +2,9 @@ module ministd.math;
 
 @safe pure nothrow @nogc:
 
+T abs(T)(const T value)
+    => value >= 0 ? value : -value;
+
 T clamp(T)(const T value, const T min, const T max)
 {
     if (value < min)
@@ -9,4 +12,25 @@ T clamp(T)(const T value, const T min, const T max)
     if (value > max)
         return max;
     return value;
+}
+
+bool inRange(string spec = "[[", T)(const T value, const T lower, const T upper)
+{
+    static assert(spec.length == 2, "Range spec must have 2 characters");
+    static foreach (c; spec)
+        static assert(c == '[' || c == ']', "Only '[' and ']' are valid spec characters");
+
+    bool lowerCheck, upperCheck;
+
+    static if (spec[0] == ']')
+        lowerCheck = lower < value;
+    else
+        lowerCheck = lower <= value;
+
+    static if (spec[1] == '[')
+        upperCheck = value < upper;
+    else
+        upperCheck = value <= upper;
+
+    return lowerCheck && upperCheck;
 }
